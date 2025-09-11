9094751b50f169817ec86e2fbb08c6ebb41b4079
Pruthviraj <pruthvipatil0058@gmail.com>
2025-09-12 00:21:16 +0530

    naru

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 // Connect MongoDB
-mongoose.connect(process.env.Database_url_atlas, {
-// mongoose.connect(process.env.MONGO_URI, {
+// mongoose.connect(process.env.Database_url_atlas, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("✅ MongoDB Connected"))
